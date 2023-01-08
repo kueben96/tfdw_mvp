238306fbfdf13996de7239110d2dc1f04ba72391
DaffyCode <107131015+DaffyCode@users.noreply.github.com>
2023-01-08 20:55:09 +0100

    first changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import { React, useEffect, useState } from 'react'
-import DonorList from './components/DonorList';
+
 
 function App() {
 
+const handleSubmit = event => {
+      event.preventDefault();
+      alert('You have submitted the form.')
+    }
 
 
 
   return (
+    
+   
+ 
     <div className="App container m-4">
+      <div className='navigator'></div>
       <div className="row">
         <div className="text-center">
-          <h1>Connecting a React Frontend to a Flask Backend.</h1>
+          <label className='spendendetails'>SPENDENDETAILS</label>
         </div>
       </div>
-      <h1>Trikot für die Welt</h1>
-
-    </div>
+      <p className='paragraph'>Wir möchten deine Spende bestmöglich erfassen. Bitte gib hier die Details deiner Spende ein.</p> 
+      <p className='meine-spende'>MEINE SPENDE</p>
+     
+     
+     
+     
+     <div className='wrapper'>
+      <form class='form-container' onSubmit={handleSubmit}>
+      <fieldset>
+         <label>
+           <p>Anzahl</p>
+           <input name="anzahl" />
+         </label>
+      </fieldset>
+       <button type="submit">ZURÜCK</button>
+       <button type='submit'>DATEN PRÜFEN</button>
+      </form>
+   </div></div>
   );
 }
 
 export default App;
+
