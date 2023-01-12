92130b1bec73f1b876d2cbe704137a372044a76e
DaffyCode <107131015+DaffyCode@users.noreply.github.com>
2023-01-12 21:05:47 +0100

    new updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import { React, useEffect, useState , useReducer} from 'react'
 
 const formReducer = (state, event) => {
+  if(event.reset) {
+    return {
+      DonationProduct: 'Empty',
+      Count: 0,
+      ColorChoice: 'Empty',
+      Gender: 'Empty',
+      Comment: 'Empty ',
+    }
+  }
   return {
     ...state,
     [event.name]: event.value
@@ -34,12 +43,20 @@ function App() {
 
    setTimeout(() => {
      setSubmitting(false);
+     setFormData({
+      reset: true
+    })
    }, 3000)
  }
  const handleChange = event => {
+  const isRadio = event.target.type === 'radio';
+   
+     
   setFormData({
     name: event.target.name,
     value: event.target.value,
+    value: isRadio ? event.target.value : event.target.value,
+
   });
 }
 
@@ -48,7 +65,7 @@ function App() {
     
  
     <div className="App container m-4">
- <nav className="nav">
+ <nav className="nav" onChange={handleChange}>
         <a href="/" className="logo">logo</a>
 
         <div className="hamburger" id='hamburger'>
@@ -83,52 +100,64 @@ function App() {
         <div className='para'>
            <p className='paragraph'>Wir möchten deine Spende bestmöglich erfassen. Bitte gib hier die Details deiner Spende ein.</p> 
         </div> 
+        {submitting &&
+        <div className='valuessubmitted'>
+          You are submitting the following:
+          <ul>
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+            ))}
+          </ul>
+        </div>
+      }
            <p className='meine-spende'>MEINE SPENDE</p>
       </div>
+
+
       {/* enctype="multipart/form-data" */}
       <div className='formdiv'>
          <form  className='form-container' onSubmit={handleSubmit} >
-         <fieldset>
+         <fieldset disabled={submitting}>
          <label>
            <p>Was möchtest du spenden?</p>
-           <select className="donationproduct" onChange={handleChange} required>
+           <select name='DonationProduct' className="donationproduct" onChange={handleChange} required>
                <option value="" selected disabled hidden>Wähle Art</option>
-               <option value="d1">Trikot in Kindergröße</option>
-               <option value="d2">Trikot in Erwachsenengröße</option>
-               <option value="d3">Trikotsatz (Trikot + Hose)</option>
-               <option value="d4">Leibchen</option>
-               <option value="d5">Fußballschuhe (Paar)</option>
-               <option value="d6">Torwarthandschuhe (Paar)</option>
-               <option value="d7">Bälle</option>
+               <option value="Kindergröße">Trikot in Kindergröße</option>
+               <option value="Erwachsenengröß">Trikot in Erwachsenengröße</option>
+               <option value="Trikotsatz">Trikotsatz (Trikot + Hose)</option>
+               <option value="Leibchen">Leibchen</option>
+               <option value="Fußballschuhe">Fußballschuhe (Paar)</option>
+               <option value="Torwarthandschuhe">Torwarthandschuhe (Paar)</option>
+               <option value="Bälle">Bälle</option>
              </select>
           </label>
 
          <label>
            <p>Anzahl</p>
-           <input className='anzahl' type="number" name="count" min='0' placeholder='Nr.' onChange={handleChange} step="1" required/>
+           <input className='anzahl' type="number" name="Count" min='0' placeholder='Nr.' onChange={handleChange} step="1" required/>
          </label>
 
          
            <p>Farbe</p>
-          <div className='colorchoice' onChange={handleChange}>
-          <label><input type="radio" id='radio1' name="testRadioSet" value="1"/></label>
-          <label><input type="radio" id='radio2' name="testRadioSet" value="2"/> </label>
-          <label><input type="radio" id='radio3' name="testRadioSet" value="3"/></label>
-          <label><input type="radio" id='radio4' name="testRadioSet" value="4"/> </label>
-          <label><input type="radio" id='radio5' name="testRadioSet" value="5"/></label>
-          <label><input type="radio" id='radio6' name="testRadioSet" value="6"/> </label>
-          <label><input type="radio" id='radio7' name="testRadioSet" value="7"/></label>
-          <label><input type="radio" id='radio8' name="testRadioSet" value="8"/> </label>
+          <div name='ColorChoice' className='colorchoice' onChange={handleChange}>
+          <label><input type="radio" id='radio1' name="testRadioSet" value="Green"/></label>
+          <label><input type="radio" id='radio2' name="testRadioSet" value="Yellow"/> </label>
+          <label><input type="radio" id='radio3' name="testRadioSet" value="Black"/></label>
+          <label><input type="radio" id='radio4' name="testRadioSet" value="White"/> </label>
+          <label><input type="radio" id='radio5' name="testRadioSet" value="Red"/></label>
+          <label><input type="radio" id='radio6' name="testRadioSet" value="Gray"/> </label>
+          <label><input type="radio" id='radio7' name="testRadioSet" value="Blue"/></label>
+          <label><input type="radio" id='radio8' name="testRadioSet" value="Orange"/> </label>
                 
             </div>  
            
             <label>
            <p>Schnitt</p>
-           <select  className='prodgender' onChange={handleChange} required>
+           <select  name='Gender' className='prodgender' onChange={handleChange} required>
                <option value="" selected disabled hidden>Wähle Schnit</option>
-               <option value="d1">unisex</option>
-               <option value="d2">male</option>
-               <option value="d3">female</option>
+               <option value="unisex">unisex</option>
+               <option value="male">male</option>
+               <option value="female">female</option>
               
              
            </select>
@@ -136,25 +165,25 @@ function App() {
          <label>
          <p>Möchtest du uns noch weitere Infos mitteilen?</p>
          
-          <textarea className='commentbox' type="text" id="comment-box" placeholder="Schreibe eine Nachricht"></textarea>
-      
+          <textarea name='Comment' className='commentbox' type="text" id="comment-box" placeholder="Schreibe eine Nachricht" onChange={handleChange}></textarea>
+         
           </label>  
           <label>
             <p>Falls du ein Foto hast, füge es gerne hinzu. </p>
-          <input type="file" className='choosefile' id="file[]" multiple />
+          <input name='AttachFile' type="file" className='choosefile' id="file[]" multiple onChange={handleChange}/>
          
           </label>
        
 
       </fieldset>
-       <button type="submit">ZURÜCK</button>
-       <button type='submit'>DATEN PRÜFEN</button>
+       <button type="submit" disabled={submitting}>ZURÜCK</button>
+       <button type='submit' disabled={submitting}>DATEN PRÜFEN</button>
       </form>
    </div></div>
 
 
         <div className='bottom'>
-          <label className='bttmtext'>Teile Freude, Hoffnung und dein Trikot</label>
+          <label className='bttmtext'>TEILE FREUDE, HOFFNUNG UND DEIN TRIKOT</label>
           </div>       
    </div></div>
   );
