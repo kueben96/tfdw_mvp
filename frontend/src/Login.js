f7f47716e15f92a7e91cf525c0dd139bee6f5eb9
DaffyCode <107131015+DaffyCode@users.noreply.github.com>
2023-01-19 18:25:05 +0100

    new updates

---
+++ frontend/src/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="App ">
+    
+    <div className="loginwrap">
+        <div className="login-text-center">
+          <label className='login-titel'>SCHÖN, DASS DU DA BIST!</label>
+        </div>
+        <div className='loginpara'>
+           <p className='login-paragraph'>Bitte melde dich mit deinen Benutzerdaten an, erstelle ein Konto oder fahre als Gast fort.  </p> 
+        </div> 
+       <p className="login-text" >Ich habe ein Benutzerkonto. </p>
+
+      </div>  
+      <div className="login-form">
+   <div class="logincontainer">
+   
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label></Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label></Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <a className="login-text1" href="#">Ich habe mein Passwort vergessen.  </a>
+        </Form.Group>
+        <Button className='loginbutton' block="true"  type="submit" onSubmit={validateForm}>
+          Anmelden
+        </Button>
+        <p className="login-text2" >Ich möchte ein Benutzerkonto erstellen.</p>
+        <Button className='loginbutton' block="true"  type="submit" disabled={!validateForm()}>
+          Registrieren
+        </Button>
+      </Form>
+    </div>
+    </div>
+    </div> 
+  );
+}
