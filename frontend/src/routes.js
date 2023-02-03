418c4c0daaa37b56ada6a3b8399358fd82e23438
DaffyCode <107131015+DaffyCode@users.noreply.github.com>
2023-02-03 16:18:09 +0100

    pre signup

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,7 +4,7 @@ import Home from './components/home';
 import Login from './components/Login';
 import DonationDetails from './components/DonationDetails';
 import Layout from './components/Layout';
-
+import Register from './components/Register';
 
 function App() {
     return (
@@ -14,6 +14,7 @@ function App() {
                 <Route index element={<Home />} />
                 <Route path='/login' element={<Login />}></Route>
                 <Route path='/donation' element={<DonationDetails />}></Route>
+                <Route path='/register' element={<Register />}></Route>
             </Route>
         </Routes>
 
