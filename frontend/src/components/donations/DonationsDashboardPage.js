4466ad4df1498f6fcf4531e2a697bac2e60c1bb4
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-02-17 11:47:51 +0100

    add dashboard structure

---
+++ frontend/src/components/donations/DonationsDashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DonationsDashboardPage = () => {
+
+    const donations = [{}]
+
+    return (
+        <div>DonationsDashboard</div>
+    )
+}
+
+export default DonationsDashboardPage
