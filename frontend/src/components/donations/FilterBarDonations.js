c46f0aa3f9aab8aaf6a4a2daf1b4f900e6c7c7f2
DaffyCode <107131015+DaffyCode@users.noreply.github.com>
2023-02-23 10:41:55 +0100

    dashboard new

--- frontend/src/components/donations/FilterBarDonations.js
+++ frontend/src/components/donations/FilterBarDonations.js
@@ -1,34 +1,48 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import '../../resources/styles/dashboardfilter.css';
 
 const FilterBarDonations = () => {
     return (
         <div className='filterbar'>
-<div className='filter-up'>
-<div className="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="split-btn1">SPENDEN</button>
-  <button type="button" class="split-btn2">SUCHEN</button>
-</div>
-   <input type="search"
-    placeholder=" Search"
-    className="search"/>
-             
-</div>
-<div className='filter-down'>
-<select className='filters' >
-<option value="" disabled selected>Kategorie</option>
+             <Container> 
+                 <Row className='filter-up'>
+                      <Col sm={6} md={6}> 
+                            <div className="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="split-btn1">SPENDEN</button>
+                            <button type="button" class="split-btn2">SUCHEN</button>
+                            </div>
+                       </Col>
+                       <Col sm={6}>
+                            <input type="search"
+                             placeholder=" Search"
+                             className="search"/>
+                        </Col>
+                 </Row>
+                 <Row className='filter-down'> 
+                       <Col sm={6} md={2}>
+                            <select className='filters' >
+                            <option value="" disabled selected>Kategorie</option>
+                            </select> 
+                       </Col>
+                       <Col sm={2} >
+                            <select className='filters'>
+                            <option value="" disabled selected>Größe</option>
+                            </select> 
+                       </Col>
+                       <Col sm={2}>
+                           <select className='filters'>
+                           <option value="" disabled selected>Farbe</option>
+                           </select> 
+                       </Col>
+                       <Col sm={2}>
+                           <input type="text"
+                            placeholder="PLZ"
+                            className="filters"/> 
+                       </Col>
+                 </Row>
 
-</select>
-<select className='filters'>
-<option value="" disabled selected>Größe</option>
-</select>
-<select className='filters'>
-<option value="" disabled selected>Farbe</option>
-</select>
-<input type="text"
-    placeholder="PLZ"
-    className="filters"/>
-</div>
+             </Container>
 
         </div>
     )
