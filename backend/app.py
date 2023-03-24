cf0937a69cc9be29f722ee9a33ffd5e06c23a72d
Sarah Katharina Thien <sarahkatharina.thien@gmail.com>
2023-03-24 10:53:09 +0100

    move config to .env file

--- backend/app.py
+++ backend/app.py
@@ -21,12 +21,12 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
-    app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_USERNAME'] = 'a10678fe9a4d7c'
-    app.config['MAIL_PASSWORD'] = 'c8244bd595751b'
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+    app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+    app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+    app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS')
+    app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL')
 
     # Initialize extensions
     # To use the application instances above, instantiate with an application:
