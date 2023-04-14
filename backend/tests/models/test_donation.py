21099497d9f1d9e18735171c86aae90daaaa4c8c
Sarah Katharina Thien <sarahkatharina.thien@gmail.com>
2023-04-14 12:41:50 +0200

    adjust tests for db models user and donation

--- backend/tests/models/test_donation.py
+++ backend/tests/models/test_donation.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime
 
 from models.donation import Donation
 
@@ -9,8 +10,10 @@ def new_donation():
     Creates a new user object from the database model User.
     Returns: user object
     """
-    donation = Donation(id=1, user_id=2, donation_type="sock", number=1, color="white", cut="unisex",
-                        description="one sock to free an elf")
+    date = datetime(year=2023, month=4, day=14, hour=12, minute=23, second=0)
+    donation = Donation(id=1, user_id=2, date=date, category="jersey_kit", amount=1, size_1="adult", size_2="M",
+                        color_1="white", color_2="green", description="green and white striped jersey kit adult size M",
+                        status="offen")
     return donation
 
 
@@ -20,11 +23,28 @@ def test_new_donation(new_donation):
     WHEN a new Donation is created
     THEN check all attributes of the created Donation model object are correct
     """
+    date = datetime(year=2023, month=4, day=14, hour=12, minute=23, second=0)
 
     assert new_donation.id == 1
     assert new_donation.user_id == 2
-    assert new_donation.donation_type == "sock"
-    assert new_donation.number == 1
-    assert new_donation.color == "white"
-    assert new_donation.cut == "unisex"
-    assert new_donation.description == "one sock to free an elf"
+    assert new_donation.date == date
+    assert new_donation.category == "jersey_kit"
+    assert new_donation.amount == 1
+    assert new_donation.size_1 == "adult"
+    assert new_donation.size_2 == "M"
+    assert new_donation.color_1 == "white"
+    assert new_donation.color_2 == "green"
+    assert new_donation.description == "green and white striped jersey kit adult size M"
+    assert new_donation.status == "offen"
+
+    assert isinstance(new_donation.id, int)
+    assert isinstance(new_donation.user_id, int)
+    assert isinstance(new_donation.date, datetime)
+    assert isinstance(new_donation.category, str)
+    assert isinstance(new_donation.amount, int)
+    assert isinstance(new_donation.size_1, str)
+    assert isinstance(new_donation.size_2, str)
+    assert isinstance(new_donation.color_1, str)
+    assert isinstance(new_donation.color_2, (str, type(None)))
+    assert isinstance(new_donation.description, (str, type(None)))
+    assert isinstance(new_donation.status, str)
