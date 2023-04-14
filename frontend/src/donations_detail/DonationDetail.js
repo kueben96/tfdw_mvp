bf7470b05bfd74154d35a922c5576f842d8fac11
DaffyCode <107131015+DafinaZaka@users.noreply.github.com>
2023-04-14 11:07:54 +0200

    new updates

--- frontend/src/donations_detail/DonationDetail.js
+++ frontend/src/donations_detail/DonationDetail.js
@@ -65,8 +65,8 @@ const DonationDetail = () => {
             
           
             </div>
-            <button className='button-pink'>Confirm</button>
-            <button className='button-pink' onClick={handleGoBack}>Go Back</button>
+            <button className='button-pink'>Kontaktieren</button>
+            <button className='button-pink' onClick={handleGoBack}>Züruck</button>
             </div>
           </div>
          
@@ -85,72 +85,3 @@ const DonationDetail = () => {
 
 
 
-
-
-// import React from 'react';
-// import { useLocation, useNavigate } from 'react-router-dom';
-
-// const ConfirmationCards = () => {
-//   const location = useLocation();
-//   const navigate = useNavigate();
-//   const donation = location.state?.donation;
- 
-//   const handleConfirm = () => {
-//     // TODO: Add logic to confirm donation
-//     navigate('/');
-//   };
-
-//   const handleGoBack = () => {
-//     navigate('/');
-//   };
-
-//   if (!donation) {
-//     return <p>No donation data available</p>;
-// }
-
-// // Render the confirmation page with the donation data
-// return (
-//     <div>
-//         <h1>Confirm Donation</h1>
-//         <p>Category: {donation.category}</p>
-//         <p>Amount: {donation.amount} Stück</p>
-//         <p>PLZ: {donation.zip_code}</p>
-//         <button>Confirm</button>
-//         <button>Go Back</button>
-//     </div>
-// );
-// };
-
-// export default ConfirmationCards;
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import { useLocation } from 'react-router-dom';
-
-// const ConfirmationCards = () => {
-//     const location = useLocation();
-//     const donation = location.state ? location.state.donation : null;
-    
-//     return (
-//         <div>
-//             <h1>Confirmation Cards</h1>
-//             {donation && (
-//                 <> <h6>h</h6>
-//                     <p>{donation.category}</p>
-//                     <p>{donation.amount}</p>
-//                     <p>{donation.zip_code}</p>
-//                 </>
-//             )}
-//         </div>
-//     )
-// }
-
-// export default ConfirmationCards;
