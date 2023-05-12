8c4a8364155ace945aa91374110bfe6b74ac6564
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-05-12 14:32:43 +0200

    remove unncessary donation request detail

--- frontend/src/components/donations_recipient/RecipientsDashboard.js
+++ frontend/src/components/donations_recipient/RecipientsDashboard.js
@@ -3,9 +3,15 @@ import { Container, Row, Col } from 'react-bootstrap';
 import '../../resources/styles/dashboard.css';
 import RecipientDashboardCards from './RecipientDashboardCards';
 import DashboardHeader from '../ui/DashboardHeader';
+import { useNavigate } from 'react-router-dom';
 
 
 const RecipientsDashboard = () => {
+
+    const navigate = useNavigate()
+    const onCreateRequestClick = () => {
+        navigate('/donation_request/new')
+    }
 
     return (
         <div>
@@ -14,7 +20,7 @@ const RecipientsDashboard = () => {
                     <DashboardHeader />
                     <div className='registerpara'>
                         <p>Erstelle ein Gesuch oder kontaktiere potenzielle Spenden.     </p>
-                        <button className='button-pink'>Gesuch erstellen</button>
+                        <button onClick={onCreateRequestClick} className='button-pink'>Gesuch erstellen</button>
                     </div>
                     <div className='articles'>
                         <RecipientDashboardCards></RecipientDashboardCards>
