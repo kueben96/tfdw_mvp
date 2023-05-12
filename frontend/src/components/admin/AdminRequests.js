35be4c55a8a27187776ffa796e19b2b9fde1dafa
DafinaZaka <107131015+DafinaZaka@users.noreply.github.com>
2023-05-12 11:24:30 +0200

    check DonationRequestDetail

--- frontend/src/components/admin/AdminRequests.js
+++ frontend/src/components/admin/AdminRequests.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import '../../resources/styles/dashboard.css';
 import DashboardHeader from '../ui/DashboardHeader';
 
